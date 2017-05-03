e5311b012e77cd79f490d7124f1e14a1796e1301
Viateur <viateur.ayegnon@gmail.com>
2017-05-03 11:47:00 +0200

    mise en place de l'horloge'

--- public/js/components/common/navbar.js
+++ public/js/components/common/navbar.js
@@ -1,14 +1,16 @@
 let navbar = {
     templateUrl: 'js/components/common/navbar.html',
-    controller: ['UsersService', '$state', function(UsersService, $state) {
+    controller: ['UsersService', '$state','$interval', function(UsersService, $state, $interval) {
         'use strict'
+        var moment = require('moment');
+
         angular.extend(this, {
             $onInit() {
                 UsersService.getCurrent().then((user) => {
-                    this.user = user
-                }).catch((err) => {
-
-                })
+                    this.user = user;
+                    this.actualHour = moment();
+                    $interval(() => {this.actualHour = moment()}, 1000);
+                }).catch((err) => {})
             },
             disconnect() {
                 UsersService.disconnect().then(() => {
